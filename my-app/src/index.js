61dce28846fa1f8ad33e7a2989a8c19ba843c168
ImMaxence <maxence.bonnici2002@gmail.com>
2025-05-24 00:07:02 +0200

    ant design config provider

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,23 @@ import './styles/index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'leaflet/dist/leaflet.css';
+import { ConfigProvider } from 'antd';
+import frFR from 'antd/locale/fr_FR';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider
+    locale={frFR}
+    theme={{
+      "token": {
+        "colorPrimary": "#FFCE10",
+
+      }
+    }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ConfigProvider>
 );
 
 serviceWorkerRegistration.register();
