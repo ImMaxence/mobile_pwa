9491decd67544649ddfad6e8e5fd012302641fc0
ImMaxence <maxence.bonnici2002@gmail.com>
2025-05-29 22:33:14 +0200

    galaxy btn

---
+++ src/components/GalaxyButton.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import '../styles/code/galaxyBtn.css';
+
+const GalaxyButton = ({ text = "Explore", onClick }) => {
+    useEffect(() => {
+        const random = (min, max) =>
+            Math.floor(Math.random() * (max - min + 1) + min);
+        const particles = document.querySelectorAll('.galaxy-button .star');
+
+        particles.forEach((p) => {
+            p.setAttribute(
+                'style',
+                `
+        --angle: ${random(0, 360)};
+        --duration: ${random(6, 20)};
+        --delay: ${random(1, 10)};
+        --alpha: ${random(40, 90) / 100};
+        --size: ${random(2, 6)};
+        --distance: ${random(40, 200)};
+      `
+            );
+        });
+    }, []);
+
+    return (
+        <div className="galaxy-button just_sha">
+            <button onClick={onClick}>
+                <span className="spark"></span>
+                <span className="backdrop"></span>
+
+                <span className="galaxy__container">
+                    {Array.from({ length: 4 }).map((_, i) => (
+                        <span key={`static-${i}`} className="star star--static"></span>
+                    ))}
+                </span>
+
+                <span className="galaxy">
+                    <span className="galaxy__ring">
+                        {Array.from({ length: 20 }).map((_, i) => (
+                            <span key={`ring-${i}`} className="star"></span>
+                        ))}
+                    </span>
+                </span>
+
+                {/* Nouveau wrapper autour du texte */}
+                <span className="galaxy-content">
+                    <span className="text">{text}</span>
+                </span>
+            </button>
+
+            <div className="bodydrop"></div>
+        </div>
+    );
+};
+
+export default GalaxyButton;
